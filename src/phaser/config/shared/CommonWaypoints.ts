63ce376201a3dd210afc2741a94c7cbb34d37c54
Robin Li <lirobinxc@gmail.com>
2022-12-24 22:36:42 -0500

    feat: add CommonWaypoints

--- src/phaser/config/shared/CommonWaypoints.ts
+++ src/phaser/config/shared/CommonWaypoints.ts
@@ -7,4 +7,119 @@ export const CommonWaypointList: WaypointDataCommon[] = [
     relativeCoord: new Phaser.Math.Vector2(0.201, 0.899),
     type: AcType.Jet,
   },
+  {
+    name: 'AVSEP',
+    relativeCoord: new Phaser.Math.Vector2(0.198, 0.166),
+    type: AcType.Jet,
+  },
+  {
+    name: 'BETES',
+    relativeCoord: new Phaser.Math.Vector2(0.279, 1),
+    type: AcType.Jet,
+  },
+  {
+    name: 'DEDKI',
+    relativeCoord: new Phaser.Math.Vector2(1, 0.606),
+    type: AcType.Jet,
+  },
+  {
+    name: 'DUSOM',
+    relativeCoord: new Phaser.Math.Vector2(0.383, 0.932),
+    type: AcType.Jet,
+  },
+  {
+    name: 'EBKIN',
+    relativeCoord: new Phaser.Math.Vector2(0.445, 0.055),
+    type: AcType.Jet,
+  },
+  {
+    name: 'IKLEN',
+    relativeCoord: new Phaser.Math.Vector2(0.538, 0.106),
+    type: AcType.Jet,
+  },
+  {
+    name: 'KEPTA',
+    relativeCoord: new Phaser.Math.Vector2(1, 0.964),
+    type: AcType.Jet,
+  },
+  {
+    name: 'KISEP',
+    relativeCoord: new Phaser.Math.Vector2(0.436, 0),
+    type: AcType.Jet,
+  },
+  {
+    name: 'LETOR',
+    relativeCoord: new Phaser.Math.Vector2(0, 0.898),
+    type: AcType.Jet,
+  },
+  {
+    name: 'NADUM',
+    relativeCoord: new Phaser.Math.Vector2(0.879, 0.741),
+    type: AcType.Prop,
+  },
+  {
+    name: 'NOSIK',
+    relativeCoord: new Phaser.Math.Vector2(0, 0.179),
+    type: AcType.Prop,
+  },
+  {
+    name: 'NUGOP',
+    relativeCoord: new Phaser.Math.Vector2(0.051, 0),
+    type: AcType.Prop,
+  },
+  {
+    name: 'OAKVL',
+    relativeCoord: new Phaser.Math.Vector2(0.318, 0.91),
+    type: AcType.Jet,
+  },
+  {
+    name: 'RIGUS',
+    relativeCoord: new Phaser.Math.Vector2(0.369, 1),
+    type: AcType.Jet,
+  },
+  {
+    name: 'RIKEM',
+    relativeCoord: new Phaser.Math.Vector2(0.826, 0.811),
+    type: AcType.Jet,
+  },
+  {
+    name: 'SEDOG',
+    relativeCoord: new Phaser.Math.Vector2(0.599, 0.167),
+    type: AcType.Jet,
+  },
+  {
+    name: 'TANGI',
+    relativeCoord: new Phaser.Math.Vector2(0.735, 0),
+    type: AcType.Jet,
+  },
+  {
+    name: 'TEVAD',
+    relativeCoord: new Phaser.Math.Vector2(1, 0.806),
+    type: AcType.Prop,
+  },
+  {
+    name: 'TESUK',
+    relativeCoord: new Phaser.Math.Vector2(1, 0.428),
+    type: AcType.Prop,
+  },
+  {
+    name: 'TONNY',
+    relativeCoord: new Phaser.Math.Vector2(0.534, 0),
+    type: AcType.Jet,
+  },
+  {
+    name: 'TULEK',
+    relativeCoord: new Phaser.Math.Vector2(0.019, 0.342),
+    type: AcType.Jet,
+  },
+  {
+    name: 'URSAL',
+    relativeCoord: new Phaser.Math.Vector2(0.122, 0.254),
+    type: AcType.Jet,
+  },
+  {
+    name: 'VERDO',
+    relativeCoord: new Phaser.Math.Vector2(1, 0.543),
+    type: AcType.Jet,
+  },
 ];
