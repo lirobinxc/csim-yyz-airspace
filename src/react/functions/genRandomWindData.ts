30006661abf4102816cf3ebed095be47764b8673
Robin Li <lirobinxc@gmail.com>
2023-04-18 19:40:11 -0400

    update: more wind directions

--- src/react/functions/genRandomWindData.ts
+++ src/react/functions/genRandomWindData.ts
@@ -6,19 +6,19 @@ export function genRandomWindData(): SimOptions['windData'] {
 
   return {
     Radar06sScene: {
-      direction: _.sample([40, 50, 60, 70, 80]) || 60,
+      direction: _.sample([20, 30, 40, 50, 60, 70, 80, 90, 100]) || 60,
       speed: _.sample(windSpeeds) || 20,
     },
     Radar15sScene: {
-      direction: _.sample([130, 140, 150, 160, 170]) || 150,
+      direction: _.sample([110, 120, 130, 140, 150, 160, 170, 180, 190]) || 150,
       speed: _.sample(windSpeeds) || 20,
     },
     Radar24sScene: {
-      direction: _.sample([220, 230, 240, 250, 260]) || 240,
+      direction: _.sample([200, 210, 220, 230, 240, 250, 260, 270, 280]) || 240,
       speed: _.sample(windSpeeds) || 20,
     },
     Radar33sScene: {
-      direction: _.sample([310, 320, 330, 340, 350]) || 330,
+      direction: _.sample([290, 300, 310, 320, 330, 340, 350, 360, 10]) || 330,
       speed: _.sample(windSpeeds) || 20,
     },
   };
